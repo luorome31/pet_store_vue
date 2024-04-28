0198a70272e2f896ace4c18dfb2e696b242a019b
wx277 <313137855@qq.com>
2024-04-29 00:41:14 +0800

    登录页面小修改

---
+++ src/stores/token.js
@@ -0,0 +1,18 @@
+import {defineStore} from 'pinia'
+import {ref} from 'vue'
+// 需要两个参数，第一是名字具有唯一性，第二个参数是函数，函数的内部可以定义状态的所有内容
+export const useTokenStore =defineStore('token',()=>{
+    //定义状态的内容
+    //1.响应式变量
+    const token=ref('')
+    //2.定义一个函数，修改token的值,
+    const setToken=(newToken)=>{
+        token.value=newToken
+    }
+    const removeToken=()=>{
+        token.value=''
+    }
+    return {
+        token,setToken,removeToken
+    }
+});
