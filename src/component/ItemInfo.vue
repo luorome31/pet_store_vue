b0fbf0de82fdc70788594c983f5ba7e166a1ebd8
luorome31 <2743057396@qq.comgit config --global user.email 2743057396@qq.com>
2024-04-30 00:34:05 +0800

    商品详情、购物车模块进行网络请求测试

--- src/component/ItemInfo.vue
+++ src/component/ItemInfo.vue
@@ -3,20 +3,19 @@
         <div class="card mb-3 col-10 offset-1 mt-5 ">
             <div class="row g-0 justify-content-center align-items-center">
                 <div class="col-md-4">
-                    <img src="@/assets/image1.jpeg" alt="My Image">
-                    <img :src="`@/assets/${itemDetail.images}`" class="img-fluid rounded-start m-1 rounded ms-4" alt="...">
+                    <img :src="itemDetail && itemDetail.image ? imageUrl: '@/assets/image1.jpeg'" alt="My Image">
                 </div>
                 <div class="col-md-8 ">
                     <div class="card-body ms-5">
                         <h5 class="card-title m-3 ">Item Information <i class="bi bi-cart-plus"></i></h5>
                         <ul class="list-group list-group-flush">
-                            <li class="list-group-item"><strong>Item ID:</strong> EST-1</li>
-                            <li class="list-group-item"><strong>Product ID:</strong> FI-SW-01</li>
-                            <li class="list-group-item"><strong>Description:</strong> Large Fresh Water fish from Japan.
+                            <li class="list-group-item"><strong>Item ID: </strong> {{ itemDetail ? itemDetail.item_id : '' }}</li>
+                            <li class="list-group-item"><strong>Product ID: </strong> {{ itemDetail ? itemDetail.product_id : '' }}</li>
+                            <li class="list-group-item"><strong>Description:</strong> {{ itemDetail ? itemDetail.description : '' }}
                             </li>
-                            <li class="list-group-item"><strong>Manufacturer:</strong> XYZ Pets</li>
-                            <li class="list-group-item"><strong>Price:</strong> $199.99</li>
-                            <li class="list-group-item"><strong>Stock:</strong> 50</li>
+                            <li class="list-group-item"><strong>Manufacturer:</strong> {{ itemDetail ? itemDetail.manufacturer : '' }}</li>
+                            <li class="list-group-item"><strong>Price:</strong> {{ itemDetail ? itemDetail.price : '' }}</li>
+                            <li class="list-group-item"><strong>Stock:</strong> {{ itemDetail ? itemDetail.stock : '' }}</li>
                             <li class="list-group-item">
                                 <router-link to="/cart" type="button" class="btn btn-primary">
                                     add to cart
@@ -33,12 +32,14 @@
 
 <script setup>
 import img from "@/assets/image1.jpeg";
-import { onBeforeMount, ref } from "vue";
+import { computed, onBeforeMount, ref } from "vue";
 import axios from "axios";
 import { useRoute } from "vue-router";
 
 let itemDetail = ref(null);
-
+let imageUrl = computed(()=>{
+    return "/src/assets/"+itemDetail.value.image;
+})
 onBeforeMount(() => {
     const route = useRoute();
     const item_id = route.params.id;
